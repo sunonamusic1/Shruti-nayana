6ac6d8b6c9b3bdfdc8dc53ff70ed50aacc1859b4
shizukamusicbot1 <sachinheroko25@gmail.com>
2025-05-26 21:10:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = os.getenv("HEROKU_API_KEY")
 # 🔄 Git & Update Settings
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/NoxxOP/ShrutiMusic")
+UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/shizukamusicbot1/ShrutiMusic")
 UPSTREAM_BRANCH = os.getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = os.getenv("GIT_TOKEN", None)
 
@@ -40,8 +40,8 @@ GIT_TOKEN = os.getenv("GIT_TOKEN", None)
 # 🔗 Support Links
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/ShrutiBots")
-SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/ShrutiBotSupport")
+SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/MUSIC_BOT_UPDATE")
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/MUSIC_BOT_UPDATE")
 PRIVACY_LINK = os.getenv("PRIVACY_LINK", "https://graph.org/Privacy-Policy-05-01-30")
 
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -85,18 +85,18 @@ AUTO_LEAVING_ASSISTANT = bool(os.getenv("AUTO_LEAVING_ASSISTANT", False))
 # 🖼️ Image URLs (Can be customized)
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-START_IMG_URL = os.getenv("START_IMG_URL", "https://files.catbox.moe/eehxb4.jpg")
-PING_IMG_URL = os.getenv("PING_IMG_URL", "https://files.catbox.moe/eehxb4.jpg")
-PLAYLIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/eehxb4.jpg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/eehxb4.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SOUNCLOUD_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-YOUTUBE_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
+START_IMG_URL = os.getenv("START_IMG_URL", "https://graph.org/file/51454330d5d9d55bcae5c-f3aab69166e2342036.jpg")
+PING_IMG_URL = os.getenv("PING_IMG_URL", "https://graph.org/file/51454330d5d9d55bcae5c-f3aab69166e2342036.jpg")
+PLAYLIST_IMG_URL = "https://graph.org/file/51454330d5d9d55bcae5c-f3aab69166e2342036.jpg"
+STATS_IMG_URL = "https://graph.org/file/51454330d5d9d55bcae5c-f3aab69166e2342036.jpg"
+TELEGRAM_AUDIO_URL = "https://graph.org/file/51454330d5d9d55bcae5c-f3aab69166e2342036.jpg"
+TELEGRAM_VIDEO_URL = "https://graph.org/file/51454330d5d9d55bcae5c-f3aab69166e2342036.jpg"
+STREAM_IMG_URL = "https://graph.org/file/51454330d5d9d55bcae5c-f3aab69166e2342036.jpg"
+SOUNCLOUD_IMG_URL = "https://graph.org/file/51454330d5d9d55bcae5c-f3aab69166e2342036.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/51454330d5d9d55bcae5c-f3aab69166e2342036.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/51454330d5d9d55bcae5c-f3aab69166e2342036.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/51454330d5d9d55bcae5c-f3aab69166e2342036.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/51454330d5d9d55bcae5c-f3aab69166e2342036.jpg"
 
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # 🔐 User & Bot State Stores
